6adb453e60e2400e04b70a2815c30b96ada04691
yashikathakur3 <yashikathakur944@gmail.com>
2025-02-04 18:51:19 +0530

    Solution to Check Even or Odd

---
+++ Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    int r = a/2;
+    r==0?printf("Even"):printf("Odd");
+    return 0;
+}
