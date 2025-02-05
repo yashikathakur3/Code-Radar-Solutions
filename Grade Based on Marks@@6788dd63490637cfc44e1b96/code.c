dfad499b324dc703658002cfb06b4e4781402635
yashikathakur3 <yashikathakur944@gmail.com>
2025-02-05 18:55:42 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -13,14 +13,14 @@ int main() {
     else if ((a>=80)&&(a<90)){
         printf("B");
     } 
-    else if((a>=70)&&(<80)){
+    else if((a>=70)&&(a<80)){
         printf("C");
-    }/*
+    }
     else if((a>=60)&&(a<70)){
         printf("D");
     }
     else {
         printf("F");
-    } */
+    } 
     return 0;
 }
