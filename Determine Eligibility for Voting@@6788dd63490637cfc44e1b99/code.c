4511a097f1fbc56895cbfc80cdce8c78e466ce3e
yashikathakur3 <yashikathakur944@gmail.com>
2025-02-06 18:09:02 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int age;
+    scanf("%d",&age);
+    if ((a>=18)){
+        printf("Eligible");
+    }
+    else{
+        printf("Not Eligible");
+    }
+    return 0;
+}
